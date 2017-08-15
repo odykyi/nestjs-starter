3fd26cc1fba30015f614712bd23083d92ddee05b
odykyi <odykyi@luxoft.com>
2017-08-15 18:57:43 +0300

    update starter kit

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
+import { NestFactory } from '@nestjs/core';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 import { ApplicationModule } from './modules/app.module';
 
-const instance = express();
-instance.use(bodyParser.json());
+const app = express();
 
-const app = NestFactory.create(ApplicationModule, instance);
-app.then((instance) => instance.listen(6000, () => console.log('Application is listening on port 6000')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(cookieParser());
+
+const nest = NestFactory.create(ApplicationModule, app);
+nest.then((a) => a.listen(6000, () => console.log('Application is listening on port 6000')));
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,26 +1,41 @@
 import { Controller, Get, Post, Request as Req, Response as Res, Param, Body, HttpStatus } from '@nestjs/common';
-import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {
 
-    constructor(private usersService: UsersService) {}
-
-    @Get()
-    public async getAllUsers(@Res() res) {
-        const users = await this.usersService.getAllUsers();
-        return res.status(HttpStatus.OK).json(users);
-    }
-
-    @Get('/:id')
-    public async getUser(@Res() res, @Param('id') id) {
-        const user = await this.usersService.getUser(id);
-        return res.status(HttpStatus.OK).json(user);
-    }
-
-    @Post()
-    public async addUser(@Res() res, @Body('user') user) {
-        const msg = await this.usersService.addUser(user);
-        return res.status(HttpStatus.CREATED).json(msg);
+@Controller()
+export  class UsersController {
+    @Get('users')
+    getAllUsers() {
+        return [];
     }
 }
+
+
+
+
+
+// import { Controller, Get, Post, Request as Req, Response as Res, Param, Body, HttpStatus } from '@nestjs/common';
+// import { UsersService } from './users.service';
+//
+// @Controller('users')
+// export class UsersController {
+//
+//     constructor(private usersService: UsersService) {}
+//
+//     @Get()
+//     public async getAllUsers(@Res() res) {
+//         const users = await this.usersService.getAllUsers();
+//         return res.status(HttpStatus.OK).json(users);
+//     }
+//
+//     @Get('/:id')
+//     public async getUser(@Res() res, @Param('id') id) {
+//         const user = await this.usersService.getUser(id);
+//         return res.status(HttpStatus.OK).json(user);
+//     }
+//
+//     @Post()
+//     public async addUser(@Res() res, @Body('user') user) {
+//         const msg = await this.usersService.addUser(user);
+//         return res.status(HttpStatus.CREATED).json(msg);
+//     }
+// }
